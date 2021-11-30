7fb1e8aee72040bbdc3290776100b6a65f407bb8
Ruan Miguel <ruanmiguelps@gmail.com>
2021-11-30 14:49:17 -0300

    create DTO

---
+++ src/products/dto/product.dto.ts
@@ -0,0 +1,7 @@
+export class createProductDTO {
+    name:string
+    description:string
+    imageUrl:string
+    price:number
+    createAt:Date
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+
+    @Post()
+    createPost(@Res() res, @Body() createProductDTO) {
+        return res.status(HttpStatus.OK).json({
+            message:"received"
+        })
+    } 
+
+}
